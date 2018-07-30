3d13aee8d9fb024b7810f9c83d1cd2cfc8a6a40b
Alex <bajskorvspel@hotmail.com>
2018-07-30 14:34:39 +0200

    Added a OnTimerTicked delegate
    Added an OnTimerTicked delegate. This also resulted on the Timer constructor as well as the TimerManager.CreateTimer function changing. You now have to cast the second parameter (the delegate function) to the type you want it to be.

--- TimerManager.cs
+++ TimerManager.cs
@@ -8,153 +8,181 @@ namespace TheShroom // Author namespace
         /// <summary>
         /// Handles creation and destruction of all the created timers as well as updates them each frame.
         /// </summary>
-	public class TimerManager : MonoBehaviour // The only reason we derive from MonoBehaviours to get access to the Update function. (probably a bad idea)
-	{
+		public class TimerManager : MonoBehaviour // The only reason we derive from MonoBehaviours to get access to the Update function. (probably a bad idea)
+		{
             /// <summary>
             /// A timer which invokes a delegate when it has finished counting down.
             /// This should not be instantiated directly, use TimerManager.CreateTimer() instead.
             /// </summary>
-	    public class Timer
-	    {
+	    	public class Timer
+	    	{
                 /// <summary>
                 /// Delegate type that onTimerFinished uses.
                 /// </summary>
-		public delegate void OnTimerFinished();
+				public delegate void OnTimerFinished();
 
                 /// <summary>
                 /// Gets invoked when the timer has finished counting down. This is set by the user.
                 /// </summary>
-		private OnTimerFinished onTimerFinished;
+				private OnTimerFinished onTimerFinished;
+
+				/// <summary>
+				/// Delegate type that onTimerTicked uses.true
+				/// </summary>
+				public delegate void OnTimerTicked();
+
+				/// <summary>
+				/// Gets invoked every time the timer ticks. (once every frame)
+				/// </summary>
+				public OnTimerTicked onTimerTicked;
 
                 /// <summary>
                 /// The time that has elapsed since the timer started counting. (in seconds)
                 /// </summary>
-		private float elapsedTime;
+				private float elapsedTime;
 
                 /// <summary>
                 /// The amount of time (in seconds) the timer should wait before finishing and invoking.
                 /// </summary>
-		private float waitTime;
+				private float waitTime;
 
                 /// <summary>
                 /// If true, the timer is paused and will not count down.
                 /// </summary>
-		private bool paused;
+				private bool paused;
 
                 /// <summary>
                 /// If true, the timer has finished.
                 /// </summary>
-		private bool finished;
+				private bool finished;
 
                 /// <summary>
                 /// Creates and initializes a new instance of a Timer.
                 /// </summary>
-		internal Timer(float waitTime, Timer.OnTimerFinished onTimerFinished, bool startNow = true)
-		{
-		    SetWaitTime(waitTime);
-		    this.onTimerFinished = onTimerFinished;
+				internal Timer(float waitTime, Timer.OnTimerFinished onTimerFinished = null, bool startNow = true, Timer.OnTimerTicked onTimerTicked = null)
+				{
+		    		SetWaitTime(waitTime);
+		    		this.onTimerFinished = onTimerFinished;
+					this.onTimerTicked = onTimerTicked;
 					
-		    if (startNow)
-		    {
-			Start();
-		    }
-		    else
-		    {
-			Pause();
-		    }
-		}
+		    		if (startNow)
+		    		{
+						Start();
+		    		}
+		    		else
+		    		{
+						Pause();
+		    		}
+				}
 
-		/// <summary>
-		/// Starts the timer. Has the same functionality as Pause().
-		/// </summary>
-		public void Start()
-		{
-		    Debug.Log("Starting SimpleTimer!"); // TODO (The Shroom): Implement the actual timers.
-		}
+				/// <summary>
+				/// Starts the timer. Has the same functionality as Pause().
+				/// </summary>
+				public void Start()
+				{
+		    		Debug.Log("Starting SimpleTimer!"); // TODO (The Shroom): Implement the actual timers.
+				}
 
-		/// <summary>
-		/// Pauses and resets the timer.
-		/// </summary>
-		public void Stop()
-	        {
-		    Pause();
-		    Reset();
-		}
+				/// <summary>
+				/// Pauses and resets the timer.
+				/// </summary>
+				public void Stop()
+	        	{
+		   			Pause();
+		    		Reset();
+				}
 
-		/// <summary>
-		/// Resets the counter of the timer.
-		/// </summary>
-		public void Reset()
-		{
+				/// <summary>
+				/// Resets the counter of the timer.
+				/// </summary>
+				public void Reset()
+				{
                     finished = false;
-		    SetElapsedTime(0.0f);
-		}
+		    		SetElapsedTime(0.0f);
+				}
 
-		/// <summary>
-		/// Pauses the timer and prevents it from counting.
-		/// </summary>
-		public void Pause()
-		{
-		    paused = true;
-		}
+				/// <summary>
+				/// Pauses the timer and prevents it from counting.
+				/// </summary>
+				public void Pause()
+				{
+				    paused = true;
+				}
 
-		/// <summary>
-		/// Unpauses the timer so it starts counting again.
-		/// </summary>
-		public void Unpause()
-		{
-		    paused = false;
-		}
+				/// <summary>
+				/// Unpauses the timer so it starts counting again.
+				/// </summary>
+				public void Unpause()
+				{
+				    paused = false;
+				}
 
-		/// <summary>
-		/// Sets the current value of the timer.
-		/// </summary>
-		public void SetElapsedTime(float newTime)
-		{
-		    elapsedTime = newTime;
-		}
+				/// <summary>
+				/// Sets the current value of the timer.
+				/// </summary>
+				public void SetElapsedTime(float newTime)
+				{
+				    elapsedTime = newTime;
+				}
 
-		/// <summary>
-		/// Returns the current value of the timer.
-		/// </summary>
-		public float GetElapsedTime()
-		{
-		    return elapsedTime;
-		}
+				/// <summary>
+				/// Returns the current value of the timer.
+				/// </summary>
+				public float GetElapsedTime()
+				{
+				    return elapsedTime;
+				}
 
-		/// <summary>
-		/// Sets the amount of seconds the timer will wait before finishing.
-		/// </summary>
-		public void SetWaitTime(float newTime)
-		{
-		    waitTime = newTime;
-		}
+				/// <summary>
+				/// Sets the amount of seconds the timer will wait before finishing.
+				/// </summary>
+				public void SetWaitTime(float newTime)
+				{
+		   			waitTime = newTime;
+				}
 
-		/// <summary>
-		/// Returns the amount of seconds the timer will wait before finishing.
-		/// </summary>
-		public float GetWaitTime()
-		{
-		    return waitTime;
-		}
+				/// <summary>
+				/// Returns the amount of seconds the timer will wait before finishing.
+				/// </summary>
+				public float GetWaitTime()
+				{
+		    		return waitTime;
+				}
 
-		/// <summary>
-		/// Sets the delegate to invoke when the timer finishes.
-                /// When the timer is finished and you don't need it anymore, don't forget to call RemoveListener.
-		/// </summary>
-		public void AddListener(OnTimerFinished delgt)
-		{
-		    onTimerFinished += delgt;
-		}
+				/// <summary>
+				/// Sets the delegate to invoke when the timer finishes.
+                /// When the timer is finished and/or you don't need it anymore, don't forget to call RemoveListener.
+				/// </summary>
+				public void AddListener(OnTimerFinished delgt)
+				{
+		    		onTimerFinished += delgt;
+				}
+
+				/// <summary>
+				/// Sets the delegate to invoke when the timer ticks.
+				/// When the timer has finished and/or you don't need it anymore, don't forget to call RemoveListener.
+				/// </summary>
+				public void AddListener(OnTimerTicked delgt)
+				{
+					onTimerTicked += delgt;
+				}
 
                 /// <summary>
                 /// Removes the specified delegate from the delegate invokation list.
-                /// This should be called in cases where you want to unsibscribe a function
+                /// This should be called in cases where you want to unsubscribe a function.
                 /// </summary>
                 public void RemoveListener(OnTimerFinished delgt)
                 {
                     onTimerFinished -= delgt;
                 }
+
+				/// <summary>
+				/// Removes the specified delegate from the delegate invokation list.
+				/// This should be called in cases where you want to unsubscribe a function.
+				public void RemoveListener(OnTimerTicked delgt)
+				{
+					onTimerTicked -= delgt;
+				}
 
                 /// <summary>
                 /// Returns true if the timer is paused, false otherwise.
@@ -175,60 +203,78 @@ namespace TheShroom // Author namespace
                 /// <summary>
                 /// Handles the countdown of the timer as well as calls the Finish() function when the timer has counted down.
                 /// </summary>
-		internal void Update()
-		{
-		    if (!paused && !finished) // If the timer isn't paused and it hasn't finished,
-		    {
-                        // we can go ahead and count up. (This is also referred to as the countdown, even though it's not a literal countdown)
-			elapsedTime += Time.deltaTime;
-		    }
+				internal void Update()
+				{
+		    		if (!paused && !finished) // If the timer isn't paused and it hasn't finished,
+		    		{
+                    	// we can go ahead and count up. (This is also referred to as the countdown, even though it's not a literal countdown)
+						elapsedTime += Time.deltaTime;
+						InvokeTickedDelegate();
+		    		}
 
                     // If the timer hasn't finished already and the elapsed time is the same or has exceeded the waiting time,
-		    if (!finished && elapsedTime >= waitTime)
-		    {
-			Finish(); // we can go ahead and finish the timer.
-		    }
-		}
+		    		if (!finished && elapsedTime >= waitTime)
+		   		 	{
+						Finish(); // we can go ahead and finish the timer.
+		    		}
+				}
 
                 /// <summary>
                 /// Gets called when the timer has finished its countdown. Sets the state of the timer and called the invoke function.
                 /// </summary>
-		private void Finish()
-		{
-		    finished = true;
-		    InvokeFinishedDelegate(); // The timer has finished; invoke the delegate.
-		}
+				private void Finish()
+				{
+				    finished = true;
+				    InvokeFinishedDelegate(); // The timer has finished; invoke the delegate.
+				}
 
                 /// <summary>
                 /// Gets called when the timer has finished its countdown. Handles invoking the onTimerFinished delegate.
                 /// </summary>
-		private void InvokeFinishedDelegate()
-		{
-		    if (onTimerFinished != null) // Only invoke if it has a subscriber.
-		    {
-		        onTimerFinished.Invoke();
-		    }
-		    else
-		    {
-		        Debug.LogError("[SimpleTimer] Timer finished but not subscriber to the onTimerFinished delegate could be found.");
-		    }
-		}
-	    }
+				private void InvokeFinishedDelegate()
+				{
+				    if (onTimerFinished != null) // Only invoke if it has a subscriber.
+				    {
+				        onTimerFinished.Invoke();
+				    }
+				}
 
-            /// <summary>
-            /// A list of all created timers. This is the reason to why not calling DeleteTimer on a created timer causes a memory leak.
-            /// </summary>
-	    private static List<Timer> timers = new List<Timer>();
+				/// <summary>
+				/// Gets called when the timer ticks (and it isn't paused). Handles invoking the onTimerTicked delegate.
+				/// </summary>
+				private void InvokeTickedDelegate()
+				{
+					if (onTimerTicked != null)
+					{
+						onTimerTicked.Invoke();
+					}
+				}
+	    	}
 
-            /// <summary>
-            /// Creates a new timer. If startNow is set to true, it will automatically start the countdown. If not you will have to call Start().
-            /// </summary>
-	    public static Timer CreateTimer(float waitTime, Timer.OnTimerFinished onTimerFinished, bool startNow = true)
-	    {
-	        Debug.Log("Creating SimpleTimer");
-		timers.Add(new Timer(waitTime, onTimerFinished, startNow));
-		return timers[timers.Count - 1]; // Return a reference to the created timer.
-	    }
+        	/// <summary>
+        	/// A list of all created timers. This is the reason to why not calling DeleteTimer on a created timer causes a memory leak.
+        	/// </summary>
+	    	private static List<Timer> timers = new List<Timer>();
+
+        	/// <summary>
+       		/// Creates a new timer. If startNow is set to true, it will automatically start the countdown. If not you will have to call Start().
+        	/// </summary>
+	    	public static Timer CreateTimer(float waitTime, Timer.OnTimerFinished onTimerFinished = null, bool startNow = true, Timer.OnTimerTicked onTimerTicked = null)
+	    	{
+	        	Debug.Log("Creating SimpleTimer");
+				timers.Add(new Timer(waitTime, onTimerFinished, startNow, onTimerTicked));
+				return timers[timers.Count - 1]; // Return a reference to the created timer.
+	    	}
+
+			/// <summary>
+			/// Create a new timer.!-- If StartNow is set to true, it will automatically start the countdown. If not you will have to call Start().
+			/// </summary>
+			public static Timer CreateTimer(float waitTime, Timer.OnTimerTicked onTimerTicked = null, bool startNow = true, Timer.OnTimerFinished onTimerFinished = null)
+			{
+	        	Debug.Log("Creating SimpleTimer");
+				timers.Add(new Timer(waitTime, onTimerFinished, startNow, onTimerTicked));
+				return timers[timers.Count - 1]; // Return a reference to the created timer.
+			}
 
             /// <summary>
             /// Deletes the timer. This should ALWAYS be called when the timer is no longer used.
@@ -236,21 +282,21 @@ namespace TheShroom // Author namespace
             /// Note: Still trying to find a better, automatic way of removing them. At first I removed them as soon as it finished,
             ///       but if the user is trying to reset the timer and restart it after it has finished this wouldn't be possible.
             /// </summary>
-            public static void DeleteTimer(Timer timer)
+           	public static void DeleteTimer(Timer timer)
             {
-                timers.Remove(timer);
+            	timers.Remove(timer);
             }
 
             /// <summary>
-            /// Loops through all created timers and updates them.
+           	/// Loops through all created timers and updates them.
             /// </summary>
-	    private void Update()
-	    {
-		foreach (Timer t in timers) // Call update all timers so that they can add deltatime each frame.
-		{
-		    t.Update();
+	    	private void Update()
+	    	{
+				foreach (Timer t in timers) // Call update all timers so that they can add deltatime each frame.
+				{
+		    		t.Update();
+				}
+	    	}
 		}
-	    }
-	}
     }
 }
